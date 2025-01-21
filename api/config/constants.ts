3da53f0434c4c8081f12a8406e65452aa9546e18
francisihe <francis.ihejirika@gmail.com>
2025-01-21 12:36:31 +0100

    fix: adjusted port checker in constants

--- api/config/constants.ts
+++ api/config/constants.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const NODE_ENV: string = process.env.NODE_ENV || 'development';
-export const PORT: number = parseInt(process.env.PORT || '3000');
+export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // Browser manager constants
 export const MAX_PAGES: number = process.env.MAX_PAGES ? parseInt(process.env.MAX_PAGES) : 5;
